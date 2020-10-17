264f8f04db07420dd7253cfe34461e974e6bf2ed
Dave Nicolson <david.nicolson@gmail.com>
2020-10-17 22:02:00 +0200

    Fix music path

--- code/iphone/BackgroundMusic.cpp
+++ code/iphone/BackgroundMusic.cpp
@@ -515,7 +515,7 @@ void iphoneStartMusic() {
 		return;
 	}
 	char	fullName[1024];
-	sprintf( fullName, "%s/base/music/d_%s.mp3", SysIphoneGetAppDir(), currentMusicName );
+	sprintf( fullName, "%s/music/d_%s.mp3", SysIphoneGetAppDir(), currentMusicName );
 	
 	printf( "Starting music '%s'\n", fullName );
 
